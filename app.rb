d984f8cc9af875027f28cc87aded4bc8f2885b0c
Christopher Dent <christopherdent01@gmail.com>
2019-11-28 01:58:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,9 +22,10 @@ class App < Sinatra::Base
   
   
   get '/multiply/:num1/:num2' do
-    
-    @product = params[:num1].to_i * params[:num2].to_i
-    p @product 
+    num1 = params[:num1].to_i 
+    num2 = params[:num2].to_i 
+    @product = num1 * num2
+    @product 
   end
   
 
